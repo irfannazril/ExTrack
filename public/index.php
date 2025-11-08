cb4a48270796cc3d234a0907c4d771ded759ebaa
Irfan Nazril <irfannazrilasdf@gmail.com>
2025-11-09 00:19:10 +0700

    Perbaikan Main Page (design, menu, layout, content)

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
   <meta charset="UTF-8">
@@ -18,13 +18,15 @@
   <div class="dashboard-container">
     <div class="row g-0">
       <!-- Sidebar -->
-      <div class="col-lg-2 col-md-3">
+      <div class="col-lg-3">
         <div class="sidebar" id="sidebar">
           <button class="close-sidebar-btn" id="closeSidebar">
             <i class="bi bi-x"></i>
           </button>
 
-          <div class="logo">ExTrack</div>
+          <div class="logo">
+            <span class="logo-color">Ex</span>Track
+          </div>
 
           <div class="nav-item active">
             <i class="bi bi-grid"></i>
@@ -32,11 +34,11 @@
           </div>
           <div class="nav-item">
             <i class="bi bi-wallet2"></i>
-            <span>Wallets</span>
+            <span>Assets</span>
           </div>
           <div class="nav-item">
-            <i class="bi bi-tag"></i>
-            <span>Categories</span>
+            <i class="bi bi-bar-chart-line-fill"></i>
+            <span>Statistics</span>
           </div>
           <div class="nav-item">
             <i class="bi bi-gear"></i>
@@ -45,40 +47,35 @@
 
           <div class="footer-links">
             <a href="#" class="footer-link">
-              <i class="bi bi-chat-dots"></i>
-              Give feedback
+              <img class="profile" src="../img/profile.jpg"></img>
+              Profile
             </a>
             <a href="#" class="footer-link">
-              <i class="bi bi-flag"></i>
-              Report issue
+              <i class="bi bi-box-arrow-in-left"></i>
+              Logout
             </a>
           </div>
         </div>
       </div>
 
       <!-- Main Content -->
-      <div class="col-lg-10 col-md-9">
+      <div class="col-lg-9">
         <div class="main-content">
           <div class="header">
             <button class="hamburger-btn" id="hamburgerBtn">
               <i class="bi bi-list"></i>
             </button>
+            <div class="menu">Dashboard</div>
             <button class="add-wallet-btn">Add Wallet</button>
           </div>
 
           <!-- Total Money (Saldo)-->
           <div class="row mb-4">
             <div class="section-title">Total Money</div>
-            <div class="col-md-6">
+            <div class="col-lg-12">
               <div class="total-money-card">
                 <div class="total-money-amount">100.000.000</div>
                 <div class="total-money-currency">IDR</div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="total-money-card">
-                <div class="total-money-amount">897.01</div>
-                <div class="total-money-currency">EUR</div>
               </div>
             </div>
           </div>
@@ -91,10 +88,10 @@
               <div class="transaction-item">
                 <div class="transaction-left">
                   <div class="transaction-icon">
-                    <i class="bi bi-arrow-down-left"></i>
+                    <i class="bi bi-arrow-left-right"></i>
                   </div>
                   <div>
-                    <div class="transaction-amount">+38 EUR</div>
+                    <div class="transaction-amount">100.000.000 IDR</div>
                     <div class="transaction-type">Transfer</div>
                   </div>
                 </div>
@@ -106,10 +103,10 @@
               <div class="transaction-item">
                 <div class="transaction-left">
                   <div class="transaction-icon">
-                    <i class="bi bi-arrow-down-left"></i>
+                    <i class="bi bi-arrow-left-right"></i>
                   </div>
                   <div>
-                    <div class="transaction-amount">40 USD</div>
+                    <div class="transaction-amount">50.000 IDR</div>
                     <div class="transaction-type">Transfer</div>
                   </div>
                 </div>
@@ -121,11 +118,11 @@
               <div class="transaction-item">
                 <div class="transaction-left">
                   <div class="transaction-icon">
-                    <i class="bi bi-arrow-down-left"></i>
+                    <i class="bi bi-graph-up-arrow"></i>
                   </div>
                   <div>
-                    <div class="transaction-amount">+640 USD</div>
-                    <div class="transaction-type">Freelance</div>
+                    <div class="transaction-amount">+100.000 IDR</div>
+                    <div class="transaction-type">Income</div>
                   </div>
                 </div>
                 <div class="transaction-menu">
@@ -136,11 +133,11 @@
               <div class="transaction-item">
                 <div class="transaction-left">
                   <div class="transaction-icon">
-                    <i class="bi bi-arrow-down-left"></i>
+                    <i class="bi bi-graph-down-arrow"></i>
                   </div>
                   <div>
-                    <div class="transaction-amount">15 EUR</div>
-                    <div class="transaction-type">Disney Plus</div>
+                    <div class="transaction-amount">20.000 IDR</div>
+                    <div class="transaction-type">Expense</div>
                   </div>
                 </div>
                 <div class="transaction-menu">
@@ -149,60 +146,66 @@
               </div>
             </div>
 
-            <!-- Asset -->
+            <!-- Assets -->
             <div class="col-lg-4">
-              <div class="section-title">Asset</div>
+              <div class="section-title">Assets</div>
               <div class="card-balance">
                 <div class="card-label">Cash</div>
-                <div class="card-amount">675 USD</div>
+                <div class="card-amount">100.000.000 IDR</div>
               </div>
               <div class="card-balance">
-                <div class="card-label">Revolut</div>
+                <div class="card-label">Bank</div>
                 <div class="card-amount">897.01 EUR</div>
               </div>
-              <div class="card-balance">
-                <div class="card-label">Bank of America</div>
-                <div class="card-amount">4588.11 USD</div>
-              </div>
+
 
 
             </div>
 
-            <!-- All Subscriptions -->
+            <!-- Statistics -->
             <div class="col-lg-4">
-              <div class="section-title">All Subscriptions</div>
-
-              <div class="subscription-item">
-                <div class="d-flex align-items-center">
-                  <i class="bi bi-chevron-down dropdown-icon"></i>
-                  <span class="subscription-name">Bank Account Fee</span>
+              <div class="section-title">Statistics</div>
+              <div class="stats-item">
+                <div class="d-flex align-items-center mb-3">
+                  <i class="bi bi-cup-straw stats-category"></i>
+                  <span class="stats-name">Food</span>
                 </div>
-                <div class="subscription-amount">16.55 USD</div>
+                <div class="progress" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar" style="width: 25%">25%</div>
+                </div>
               </div>
 
-              <div class="subscription-item">
-                <div class="d-flex align-items-center">
-                  <i class="bi bi-check-circle-fill check-icon"></i>
-                  <span class="subscription-name">Disney Plus</span>
+              <div class="stats-item">
+                <div class="d-flex align-items-center mb-3">
+                  <i class="bi bi-car-front-fill stats-category"></i>
+                  <span class="stats-name">Transport</span>
                 </div>
-                <div class="subscription-amount">16 EUR</div>
+                <div class="progress" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar" style="width: 50%">50%</div>
+                </div>
               </div>
 
-              <div class="subscription-item">
-                <div class="d-flex align-items-center">
-                  <i class="bi bi-check-circle-fill check-icon"></i>
-                  <span class="subscription-name">Netflix</span>
+              <div class="stats-item">
+                <div class="d-flex align-items-center mb-3">
+                  <i class="bi bi-dice-6-fill stats-category"></i>
+                  <span class="stats-name">Entertainment</span>
                 </div>
-                <div class="subscription-amount">9.99 USD</div>
+                <div class="progress" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar" style="width: 75%">75%</div>
+                </div>
               </div>
 
-              <div class="subscription-item">
-                <div class="d-flex align-items-center">
-                  <i class="bi bi-check-circle-fill check-icon"></i>
-                  <span class="subscription-name">Spotify</span>
+              <div class="stats-item">
+                <div class="d-flex align-items-center mb-3">
+                  <i class="bi bi-dice-6-fill stats-category"></i>
+                  <span class="stats-name">Entertainment</span>
                 </div>
-                <div class="subscription-amount">9.99 EUR</div>
+                <div class="progress" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar" style="width: 75%">75%</div>
+                </div>
               </div>
+
+
             </div>
           </div>
         </div>
